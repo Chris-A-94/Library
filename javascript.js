b6c16feaefb8e4f67135fd6688a748ef11892fda
Chris <almeidachristian94@hotmail.com>
2024-01-26 17:59:41 -0300

    Add delete book function
    Added button and event listener that eliminates a book from selector and myLibrary array

--- javascript.js
+++ javascript.js
@@ -36,8 +36,12 @@ function addBookToSelector(book)
     })
 
     selector.appendChild(option);
+  
 
 }
+
+
+
 
 function displayBook(book)
 {
@@ -45,6 +49,7 @@ function displayBook(book)
     const author = document.getElementById('actualAuthor');
     const pages = document.getElementById('numberPages');
     const read = document.getElementById('read');
+
 
     title.textContent = book.title;
     author.textContent = book.author;
@@ -68,20 +73,31 @@ const dividedLands = new Book("Divided Lands", "Christian & Ivan", 250,false);
 songOfHope.addBookToArray(songOfHope);
 dividedLands.addBookToArray(dividedLands);
 
-addBookToSelector(myLibrary[1]);
 addBookToSelector(myLibrary[0]);
+addBookToSelector(myLibrary[1]);
 
 const openForm = document.getElementById('openForm');
 const closeForm = document.getElementById('closeForm');
 const dialog = document.querySelector("dialog");
 const bookAdder = document.getElementById('addingBook');
 const showAllBooks = document.getElementById('iteration');
+const bookDeleter = document.getElementById('eliminate');
+bookDeleter.style.backgroundColor = "grey";
+const selector = document.getElementById('books');
 
 const noDisplay = document.getElementById('default');
 const empty = new Book("","","");
 
 noDisplay.addEventListener('click',()=>{
     displayBook(empty);
+})
+
+selector.addEventListener('click',()=>{
+    const title = document.getElementById('actualTitle');
+    if(title.textContent === "")
+        bookDeleter.style.backgroundColor = "grey";
+    else
+        bookDeleter.style.backgroundColor = "red";
 })
 
 const inputTitle = document.getElementById('enterTitle');
@@ -128,4 +144,32 @@ showAllBooks.addEventListener('click',()=>{
          }
     }
     loop();
+})
+
+bookDeleter.addEventListener('click',()=>{
+    const title = document.getElementById('actualTitle');
+    if(title.textContent === "")
+        return;
+    let j;
+    for(let i = 0; i < myLibrary.length; i++)
+    {
+        if(myLibrary[i].title === title.textContent)
+            j = i;
+    }
+
+    const selector = document.getElementById('books');
+    const options = selector.children;
+
+    for (option of options)
+    {
+        if(option.textContent === myLibrary[j].title)
+            selector.removeChild(option);
+    }
+    bookDeleter.style.backgroundColor = "grey";
+    myLibrary.splice(j,1);
+
+    
+    const empty = new Book("","","");
+    displayBook(empty);
+    
 })
